12ae4d0ff25ef49cc38b14a95e7db9fd0e6a61ef
Mateusz-11 <tweak.night@gmail.com>
2023-01-15 22:06:32 +0100

    Add models

--- clothes_app/models.py
+++ clothes_app/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+TYPE_INSTITUTION = (
+   (1, "Fundacja"),
+   (2, "Organizacja pozarządowa"),
+   (3, "Zbiórka lokalna"),
+)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField(null=True, blank=True)
+    type = models.IntegerField(choices=TYPE_INSTITUTION, default=1)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+
+class Donation(models.Model):
+    quantity = models.SmallIntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=128)
+    phone_number = models.CharField(max_length=24, blank=True, null=True)
+    city = models.CharField(max_length=32)
+    zip_code = models.CharField(max_length=12)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField(null=True, blank=True)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
