1e14bfc7969c0331ba0eccedb40e967616533d43
Mateusz-11 <tweak.night@gmail.com>
2023-01-19 22:27:26 +0100

    Add placeholder to login and register forms

--- clothes_app/forms.py
+++ clothes_app/forms.py
@@ -2,11 +2,11 @@ from django import forms
 
 
 class RegisterForm(forms.Form):
-    mail = forms.EmailField(label="Email", max_length=30)
-    password = forms.CharField(label="Hasło", max_length=30, widget=forms.PasswordInput)
-    password_repeat = forms.CharField(label="Powtórz hasło", max_length=30, widget=forms.PasswordInput)
+    mail = forms.EmailField(label="Email", max_length=30, widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+    password = forms.CharField(label="Hasło", max_length=30, widget=forms.PasswordInput(attrs={'placeholder': 'Hasło'}))
+    password_repeat = forms.CharField(label="Powtórz hasło", max_length=30, widget=forms.PasswordInput(attrs={'placeholder': 'Powtórz hasło'}))
 
 
 class LoginForm(forms.Form):
-    mail = forms.EmailField(label='Email', max_length=30)
-    password = forms.CharField(label='Hasło', max_length=30, widget=forms.PasswordInput)
+    mail = forms.EmailField(label='Email', max_length=30, widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+    password = forms.CharField(label='Hasło', max_length=30, widget=forms.PasswordInput(attrs={'placeholder': 'Hasło'}))
